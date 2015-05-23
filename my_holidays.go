f44593cb70ade99c0e83ee6a0487d6fbca71e082
Tom Leach <tom@gc.io>
2015-05-22 22:20:32 -0400

    Got multiple output formats working

--- my_holidays.go
+++ my_holidays.go
@@ -2,34 +2,73 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/go-martini/martini"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"sort"
+	"strings"
+	"text/template"
 )
 
 func main() {
 	m := martini.Classic()
-	m.Get("/holidays", render)
-	log.Printf("my_holidays listening on port 8020")
+	m.Get("/holidays.(?P<format>(json|ics))", render)
+	log.Printf("my_holidays listening on port 8027")
 	err := http.ListenAndServe(":8027", m)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
 
-func render(w http.ResponseWriter, req *http.Request) {
-	generators := []HolidayGenerator{
-		NewYearsDay,
-		MartinLutherKingJuniorDay,
-		PresidentsDay,
-		MemorialDay,
-		IndependenceDay,
-		LaborDay,
-		ColumbusDay,
-		VeteransDay,
-		Thanksgiving,
-		ChristmasDay,
+func renderCal(holidays []Holiday, w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "text/calendar")
+	w.Header().Set("Content-Disposition", "attachment; filename=\"holidays.ics\"")
+	cwd, _ := os.Getwd()
+	tmpl, error := template.ParseFiles(filepath.Join(cwd, "./templates/holidays.ics"))
+	if error != nil {
+		fmt.Print(error)
+		return
+	}
+	data := struct {
+		Holidays []Holiday
+	}{
+		holidays,
+	}
+	tmpl.Execute(w, data)
+}
+
+func renderJson(holidays []Holiday, w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	json, _ := json.Marshal(holidays)
+	w.Write(json)
+}
+
+func render(w http.ResponseWriter, req *http.Request, params martini.Params) {
+	availableGenerators := map[string]HolidayGenerator{
+		"NEW": NewYearsDay,
+		"MEM": MemorialDay,
+		"LAB": LaborDay,
+		"COL": ColumbusDay,
+		"TGV": Thanksgiving,
+		"MLK": MartinLutherKingJuniorDay,
+		"PRS": PresidentsDay,
+		"IND": IndependenceDay,
+		"VET": VeteransDay,
+		"CHR": ChristmasDay,
+	}
+
+	var generators []HolidayGenerator
+	if include := req.URL.Query().Get("include"); include != "" {
+		for _, code := range strings.Split(include, ",") {
+			generators = append(generators, availableGenerators[code])
+		}
+	} else {
+		for _, v := range availableGenerators {
+			generators = append(generators, v)
+		}
 	}
 
 	var holidays []Holiday
@@ -38,6 +77,10 @@ func render(w http.ResponseWriter, req *http.Request) {
 		holidays = append(holidays, generator(10)...)
 	}
 	sort.Sort(ByDate(holidays))
-	json, _ := json.Marshal(holidays)
-	w.Write(json)
+
+	if params["format"] == "ics" {
+		renderCal(holidays, w)
+	} else {
+		renderJson(holidays, w)
+	}
 }
